a02b4a18ac6d5c06ef7d574f218e8eaa2e321107
PMT201 <PMT201@PMT3026>
2026-03-06 11:06:02 +0700

    .

---
+++ KyThuatLapTrinh/Buoi1/Buoi1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct sinhvien
+{
+	string hoten;
+	int tuoi;
+	bool gioitinh;
+	string diachi;
+};
+int main() {
+	sinhvien b;
+	b.hoten = "Nguyen Van A";
+	b.tuoi = 20;
+	b.gioitinh = true;
+	b.diachi = "Tay Ninh";
+
+	sinhvien c;
+	c.hoten = "Seele";
+	c.tuoi = 30;
+	
+	vector<sinhvien> mang;
+	mang.push_back(b);
+	mang.push_back(c);
+	for (int i = 0; i < mang.size();i++) {
+		cout << "sinh vien " << (i + 1) << endl;
+		cout << "\t+Ho Ten:" << mang[i].hoten << endl;
+		cout << "\t+ Tuoi:" << mang[i].tuoi << endl;
+
+	}
+
+
+
+
+
+	/*int a = 5;
+	int* p = &a;
+	a += 6;
+	a = *p + 2;
+	cout << *p;*/
+	/*vector <float> diemthi;
+	diemthi.push_back(5.5);
+	diemthi.push_back(4);
+	diemthi.push_back(6);
+	diemthi.pop_back();
+	for (int i = 0; i < diemthi.size(); i++) {
+		cout << diemthi[i] << endl;  
+	}*/
+}
